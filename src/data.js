99db58bdbaf1f7d6c7eafd3d5f0a7e0fb2611722
glisselisbethjm <glisse1986.jm@outlook.com>
2019-06-14 10:28:47 -0500

    info general portfolio json

---
+++ src/data.js
@@ -0,0 +1,120 @@
+let data = {
+    "imagebaseurl":"https://glisselisbeth.github.io/",
+    "name": "Glisse Jorge",
+    "role":[
+      {
+        "name":"Frontend Developer",
+        },
+      {
+        "name":"Backend Developer",
+      },
+      {
+        "name":"Bachiller en Ingeniería de Sistemas",
+      }
+    ],
+    "linkedinId":"glisselisbeth",
+    "skypeid": "glisse86",
+    "roleDescription": "Apasionada por los códigos y la tecnología",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/glisselisbeth/",
+          "className": "fas fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/glisselisbeth",
+          "className":"fas fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/glisselisbeth",
+          "className":"fas fa-twitter"
+        },
+        {
+          "name":"dev",
+          "url":"https://dev.to/glisselisbeth",
+          "className":"fas fa-dev"
+        }       
+      ],
+    "aboutme":"Soy bachiller en Ingeniería de Sistemas, terminé en Laboratoria, la carrera de Frontend Developer; y en la Universidad San Cristóbal la carrera de Ingeniería de Sistemas.  Una de mis experiencias en el área de desarrollo web y de aplicaciones, es que con cada línea de código, me esfuerzo por hacer de la web y la aplicación un lugar hermoso y con la mejor experiencia del usuario. Trabajar con un equipo multidisciplinario y bajo los enfoques de metodologías ágiles. Siempre estuve interesada en las diferentes tecnologías y los códigos, y siempre activa en continuo aprendizaje",
+    "address":"Mz. P4 Lote 16 Ampliación 1 12 de noviembre Pamplona Alta, San Juan de Miraflores",
+    "website":"https://www.glisse.co",
+    "education":[
+      {
+        "UniversityName":"Universidad Nacional San Cristóbal de Huamanga",
+        "specialization":"Bachiller en Ingeniería de Sistemas",
+        "YearOfPassing":"2005-2011"
+      },
+      {
+        "UniversityName":"Laboratoria",
+        "specialization":"Frontend Developer",
+        "YearOfPassing":"2017"
+      }
+    ],
+    "skillsDescription":"Mis habilidades",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"jQuery"
+      },
+      {
+        "skillname":"DOM"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"React Native"
+      },
+      {
+        "skillname":"AngularJS"
+      },
+      {
+        "skillname":"Angular"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Laboratoria News",
+        "description":"Caso implementado en equipo, con herramientas como gulp, sass, nodemon, express, firebase y API",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Task App",
+        "description":"Ejemplo implementado cor React y Typescript",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Yape App",
+        "description":"Aplicación Node.js con Docker y express",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Github User App",
+        "description":"Un cliente frontend de React que se comunica con la API Github GraphQL y el cliente Apollo.",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Pokedex",
+        "description":"Sitio web utilizado Api Official Pokedex con Ajax, Nodejs y Express 5",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Landing Sodimac Web",
+        "description":"Repositorio implementado en Altimea para el desarrollo de Sodimac de Alquiler de herramientas.",
+        "imgurl":"images/portfolio/project.jpg"
+      }
+    ]
+  }
+  
+  export default data
+  
