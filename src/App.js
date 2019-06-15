911d99733c6c2efe7654077e41639ee2716c1531
glisselisbethjm <glisse1986.jm@outlook.com>
2019-06-15 04:02:04 -0500

    prettier and eslinter, and error serviceworker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import "./fontello/css/fontello.css"
+import './fontello/css/fontello.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <i className="icon-up-circled" style={{ fontSize: "80px" }} />
+        <i className="icon-up-circled" style={{ fontSize: '80px' }} />
       </header>
     </div>
   );
